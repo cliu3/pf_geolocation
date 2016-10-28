4cd4ff5491309e91667be1f38e4b1c966086685a
Chang Liu <liuchangliuchang@gmail.com>
2016-10-28 15:09:32 -0400

    complete conversion of likelihood from Matlab

--- pf_geolocation/likelihood.py
+++ pf_geolocation/likelihood.py
@@ -2,21 +2,20 @@
 from __future__ import print_function
 import numpy as np
 from scipy.stats import norm
+import utils
 from config import *
+
 def likelihood(tag):
     """
     Construction of likelihood function after (Le Bris et al, 2013 eq (2))
     using daily max depth and depth where tidal signal is detected, with
     coorespoding temperature.
 
-    Translation from Matlab code likelihood_cliu.m in hmm_smast
+    Converted from Matlab code likelihood_cliu.m in hmm_smast
     """
     import netCDF4
     import scipy.io
     from my_project import my_project
-
-
-    
 
 
     # loop over day to detect tidal signal and find daily best fit
@@ -81,6 +80,14 @@ def likelihood(tag):
         ObsLh_temp_total = ObsLh_temp_calc(i, t, std_temp, day_tidal_depth_temp, day_max_depth_temp, tide, fvcom.nverts)
 
         # Release/recapture locations treatment
+        
+        # ## release location treatment
+        # xl, yl = my_project(tag['release_lon'],tag['release_lat'],'forward')
+        # dist_rl = ( (fvcom.xc-xl)**2 + (fvcom.yc-yl)**2 )**0.5
+        # rel_idx = np.argmin(dist_rl)
+        # ObsLh[0,fvcom.tri[rel_idx,:]] = 1
+
+        ## recapture location attraction likelihood
         if(tag['recap_uncertainty_km']>0):
             xr, yr = my_project(tag['recapture_lon'],tag['recapture_lat'],'forward')
             dist_r = ( (fvcom.x-xr)**2+(fvcom.y-yr)**2 )**0.5
@@ -91,18 +98,34 @@ def likelihood(tag):
         else:
             AttLh = 1
         # calculate daily likelihood distribution
-        ObsLh[i,:]=ObsLh_dep_total*ObsLh_temp_total*AttLh;
+        ObsLh[i,:]=ObsLh_dep_total*ObsLh_temp_total*AttLh
 
-    # TIDAL EXCLUSION
+    # TIDAL EXCLUSION based on RMSE
+    print('Performing tidal-based exclusion to ObsLh using RMSE of tidal signal ... ')
+
+    ## 13-hour fitting to determine eta_tag_fit
+    eta_tag_fit, day_ampli, intv_cell = tidal_detection_long(tag, tide)
+
+    ## reconstruction of FVCOM tidal signal using harmonics
+    eta1, node_idx = FVCOM_predict(fvcom, tag['dnum'], day_ampli)
+
+    ## Loop over days to calculate RMSE between tag nad FVCOM
+    rmse_con = rmse_con_calc(ndays, tide, eta1, intv_cell, eta_tag_fit, fvcom.nverts, node_idx)
+    
+    ## update ObsLh
+    #import ipdb; ipdb.set_trace()
+    ObsLh = ObsLh * rmse_con
+
+
 
     tagname = str(np.asscalar(tag['fish_id']))+'_'+tag['tag_id'][0]
     scipy.io.savemat('ObsLh'+tagname+'.mat', {'ObsLh': ObsLh, 'tide': tide})
     #return ObsLh, tide
 
+    
 
 
-
-    # recapture location attraction likelihood
+    
 
 def tidal_detection(tag):
     """
@@ -110,7 +133,7 @@ def tidal_detection(tag):
     window, and return the daily max depth and tidal depth (if applicable)
     and the associated temperature.
     """
-    from config import *
+    #from config import *
     
 
     Twindow = 5  #time window = 5 h
@@ -273,7 +296,7 @@ def std_dep_calc(fvcom, nbsn):
     std_dep=np.empty(fvcom.nverts)*np.nan
     nnodes = fvcom.nverts
     for nd in xrange(nnodes):
-        if(nd%3000 == 0) or (nd == nnodes-1): print( int((float(nd+1)/nnodes)*100),"%")
+        if(nd%3000 == 0) or (nd == nnodes-1): print('  ', int((float(nd+1)/nnodes)*100),"%")
         
         # progress output
         # if (mod(nd,500)==0):
@@ -359,3 +382,130 @@ def ObsLh_temp_calc(i, t, std_temp, day_tidal_depth_temp, day_max_depth_temp, ti
 
 
     return ObsLh_temp_total
+
+def tidal_detection_long(tag, tide):
+
+    Twindow = 13  #time window = 13 h
+    nwindow = int(np.asscalar(Twindow*3600//tag['min_intvl_seconds'])) # window size in data point numbers
+
+    ntimes = len(tag['dnum'])
+
+    int_dnum = np.floor(tag['dnum'])
+    dbeg = int_dnum[0]
+    dend = int_dnum[-1]
+    days = range(dbeg,dend+1)
+    ndays = len(days)
+
+    #p: M2 period in hours
+    p = 12.420601
+    w=2*np.pi/(p/24) # Angular frequency
+    sint = np.sin(w*tag['dnum'])
+    cost = np.cos(w*tag['dnum'])
+
+    day_ampli=np.empty(ndays)*np.nan
+    intv_cell = [None]*ndays
+    eta_tag_fit = [None]*ndays
+    print('Detecting long tidal signal...')
+    for i in range(ndays):
+        print('  day: '+str(i+1)+' of '+str(ndays))
+        days_idx=np.where(int_dnum == days[i])[0]
+        rmse=np.empty(len(days_idx))*np.nan
+        rsquare=np.empty(len(days_idx))*np.nan
+        ampli=np.empty(len(days_idx))*np.nan
+        if (days_idx[0]+nwindow > ntimes):
+            break
+
+        #move window for each data point
+        for j in range(len(days_idx)):
+            if (days_idx[j]+nwindow > ntimes): break
+            
+            intv=range(days_idx[j], min(ntimes,days_idx[j]+nwindow-1) + 1 )
+            rmse[j], rsquare[j], ampli[j] = lssinfit(np.ones(len(intv)), cost[intv], sint[intv], tag['depth'][intv])[0:3]
+            
+        
+        
+        # Find intervals with tidal information according to criteria
+        crit = (rmse<tideLV[0]) & (rsquare>tideLV[1]) & (ampli>tideLV[2]) & (ampli<tideLV[3])
+
+        #find best fit for each day and reconstruct corresponding fvcom signal
+        if np.sum(crit)>0:
+            tide[i] = 2
+            
+            idx=np.where(rmse==np.min(rmse[crit]))[0]
+            intv=range(days_idx[idx], min(ntimes,days_idx[idx]+nwindow-1)+1 )
+            intv_cell[i]=intv
+            time=tag['dnum'][intv].T[0]
+            day_ampli[i] = ampli[idx]
+            
+            eta_tag = ( tag['depth'][intv]-np.mean(tag['depth'][intv]) ).T[0]
+            
+            # nonlinear sine fit
+            eta_tag_fit[i] = utils.fit(time,eta_tag)
+            
+            
+    return eta_tag_fit, day_ampli, intv_cell
+
+def FVCOM_predict(fvcom, dnum, day_ampli):
+    """
+    Reconstruction of FVCOM tidal signal
+    """
+    from astropy.time import Time
+
+    # #Define tidal constituents and values
+    # inpcon = fvcom.comps
+
+    # only consider nodes whose range of amplitude falls in range of amplitude
+    # of fitted signal +/- ampl_buffer (in meters)
+    ampl_buffer = 0.0
+    Fr1=0.01*( fvcom.amp[:,0] - np.sum(fvcom.amp[:,1:],1) )
+    Fr1[Fr1<=0] = 0
+    Fr2=0.01*np.sum(fvcom.amp,1)
+    Tr1=max(0,min(day_ampli)-ampl_buffer)
+    Tr2=max(day_ampli)+ampl_buffer
+    node_idx = np.where( (Fr2>=Tr1) & (Fr1<=Tr2) )[0]
+
+    pha1=np.zeros( (len(node_idx),len(fvcom.comps)) )
+    amp1=np.zeros( (len(node_idx),len(fvcom.comps)) )
+    print('Reconstructing FVCOM tidal signal ... ')
+
+    tag_time = Time(dnum-678942,format='mjd',scale='utc').datetime[:,0]
+    eta1 = [None]*len(node_idx)
+    pha1 = fvcom.phase[node_idx,:]
+    amp1 = fvcom.amp[node_idx,:]*0.01 #cm to m    
+    for nd in xrange(len(node_idx)):
+        # if(nd%3000 == 0) or (nd == nnodes-1): print( int((float(nd+1)/nnodes)*100),"%")
+        if (nd+1)%500 == 0:
+            print('  node: '+str(nd+1)+'/'+str(len(node_idx))+' -- ', int((float(nd+1)/len(node_idx))*100),"%")
+
+        # create timeseries with the defined tidal harmonics info with ttide
+        eta1[nd]= utils.predict(tag_time, amp1[nd], pha1[nd])
+        
+    return eta1, node_idx
+
+
+def rmse_con_calc(ndays, tide, eta1, intv_cell, eta_tag_fit, nverts, node_idx):
+
+    print('Applying threashold for tidal fit ... ')
+    rmse_con = np.ones((ndays,nverts))
+    for i in xrange(ndays):
+        print('  Processing day: ', str(i+1), ' of ', str(ndays), '...')
+
+        # calculate rmse map
+        if (tide[i]==2):
+            rmse_eta = np.empty(nverts)*np.nan
+            
+            intv = intv_cell[i]
+            
+            for nd in xrange(len(node_idx)):
+                
+                #calculate tidal signal RMSE
+                eta1_window=eta1[nd][intv]-np.mean(eta1[nd][intv])
+                rmse_eta[node_idx[nd]]= np.sqrt(np.mean( (eta_tag_fit[i]-eta1_window)**2))
+                
+                
+            
+            thresh = np.nanmin(rmse_eta)+0.3*(np.nanmax(rmse_eta) - np.nanmin(rmse_eta))
+            rmse_con[i,:]=0
+            rmse_con[i,rmse_eta<=thresh]=1
+
+    return rmse_con
